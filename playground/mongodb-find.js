ca53e62db6c3977b3e031477ed1f390ab0d44f13
Adekunbi Adefolarin <dekunbifola100@gmail.com>
2017-08-22 17:38:51 +0100

    Init Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,29 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+   
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('599afdea739e6776a5fb2178')
+    // }).toArray().then((docs) => {
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'Adefolarin'}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    });
+
+    // db.close();
+});
